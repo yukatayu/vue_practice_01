cbf7909cf3de8a9d602efffdcf2d81b6e2889832
yukatayu <yukatayu.dev@gmail.com>
2023-11-24 13:54:52 +0900

    fix: remove built files / add: vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,14 +3,14 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const count = ref(1)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ count }} !!</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
